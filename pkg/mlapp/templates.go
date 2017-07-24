0a7ab5d993608f5fe9f667d12714479c40f28493
Nikolay Mahotkin <makhotkin@selectel.ru>
2017-07-24 17:55:52 +0300

    Callbacks

--- pkg/mlapp/templates.go
+++ pkg/mlapp/templates.go
@@ -291,7 +291,7 @@ func (c *Config) GenerateTaskResources(jobID string) ([]*kubernetes.KubeResource
 			if g.Port > 0 {
 				res.Deps = []*kubernetes.KubeResource{generateHeadlessService(g)}
 			}
-			callbacks = append(callbacks, Callback{WaitCount: g.WaitCount(), TaskName: task.Name})
+			callbacks = append(callbacks, Callback{WaitCount: g.WaitCount(), TaskName: task.Name, AcceptedCallbacks: make(map[string]int)})
 			resources = append(resources, res)
 		}
 	}
