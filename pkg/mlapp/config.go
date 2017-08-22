287717deb5a2bf0d7bc58dbc52fd0d4f79d8e27f
Alexander Gunin <guninalexander@gmail.com>
2017-08-21 18:55:28 -0700

    fix subpath

--- pkg/mlapp/config.go
+++ pkg/mlapp/config.go
@@ -183,12 +183,18 @@ func (c *Config) KubeVolumesSpec(mounts []VolumeMount) ([]v1.Volume, []v1.Volume
 			mountPath = m.MountPath
 		}
 		subPath := v.SubPath
-		if strings.HasPrefix(subPath, "/shared/") {
-			subPath = strings.TrimPrefix(subPath, "/")
-		} else if strings.HasPrefix(subPath, "/") {
-			subPath = c.Workspace + "/" + c.WorkspaceID + "/" + strings.TrimPrefix(subPath, "/")
-		} else if len(subPath) > 0 {
-			subPath = c.Workspace + "/" + c.WorkspaceID + "/" + c.Name + "/" + subPath
+		if v.NFS!=nil {
+			if strings.HasPrefix(subPath, "/shared/") {
+				subPath = strings.TrimPrefix(subPath, "/")
+			} else if strings.HasPrefix(subPath, "/") {
+				subPath = c.Workspace + "/" + c.WorkspaceID + "/" + strings.TrimPrefix(subPath, "/")
+			} else if len(subPath) > 0 {
+				subPath = c.Workspace + "/" + c.WorkspaceID + "/" + c.Name + "/" + subPath
+			}
+		} else{
+			if strings.HasPrefix(subPath, "/"){
+				subPath = strings.TrimPrefix(subPath, "/")
+			}
 		}
 		if len(m.SubPath) > 0 {
 			subPath = filepath.Join(subPath, m.SubPath)
