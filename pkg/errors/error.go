7a1bbb25d632721178f72a6cbc58076704ad8f69
Vadim <monstarnn@gmail.com>
2017-12-04 12:50:21 +0300

    lib errors

---
+++ pkg/errors/error.go
@@ -0,0 +1,87 @@
+package errors
+
+import (
+	"github.com/jinzhu/gorm"
+	"net/http"
+)
+
+const (
+	MessageUnknownError = "unknown error"
+)
+
+type Error struct {
+	Status  int    `json:"status"`
+	Message string `json:"message,omitempty"`
+}
+
+func (e *Error) Error() string {
+	if len(e.Message) == 0 {
+		return MessageUnknownError
+	}
+	return e.Message
+}
+
+func (e *Error) HttpStatus() int {
+	if e.Status <= 0 {
+		return http.StatusInternalServerError
+	}
+	return e.Status
+}
+
+func New(text string) error {
+	return NewStatus(http.StatusInternalServerError, text)
+}
+
+func NewStatus(status int, text string) error {
+	return Smart(status, text)
+}
+
+func Smart(args ...interface{}) error {
+	err := &Error{}
+	var statusSet, messageSet, errSet bool
+	for _, arg := range args {
+		switch a := arg.(type) {
+		case *Error:
+			if errSet {
+				continue
+			}
+			if !statusSet {
+				err.Status = a.Status
+			}
+			if !messageSet {
+				err.Message = a.Message
+			}
+			errSet = true
+		case error:
+			if errSet {
+				continue
+			}
+			if a == gorm.ErrRecordNotFound {
+				err.Status = http.StatusNotFound
+				if !messageSet {
+					err.Message = a.Error()
+				}
+			} else {
+				if messageSet {
+					continue
+				}
+				err.Message = a.Error()
+				messageSet = true
+			}
+			errSet = true
+		case string:
+			if messageSet {
+				continue
+			}
+			err.Message = a
+			messageSet = true
+		case int:
+			if statusSet {
+				continue
+			}
+			err.Status = a
+			statusSet = true
+		}
+	}
+	return err
+}
---
+++ pkg/errors/error_test.go
@@ -0,0 +1,99 @@
+package errors
+
+import (
+	"net/http"
+	"os"
+	"reflect"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+func TestErrorNew(t *testing.T) {
+
+	err1 := New("")
+	isEqual(MessageUnknownError, err1.Error(), t)
+	err1e := err1.(*Error)
+	isEqual(http.StatusInternalServerError, err1e.HttpStatus(), t)
+
+	err2 := New("new err")
+	isEqual("new err", err2.Error(), t)
+
+}
+
+func TestErrorNewStatus(t *testing.T) {
+
+	err1 := NewStatus(http.StatusBadRequest, "")
+	isEqual(MessageUnknownError, err1.Error(), t)
+	err1e := err1.(*Error)
+	isEqual(http.StatusBadRequest, err1e.HttpStatus(), t)
+
+	err2 := NewStatus(http.StatusBadGateway, "new err")
+	isEqual("new err", err2.Error(), t)
+	err2e := err2.(*Error)
+	isEqual(http.StatusBadGateway, err2e.HttpStatus(), t)
+
+}
+
+func TestErrorSmart(t *testing.T) {
+
+	err1 := Smart()
+	isEqual(MessageUnknownError, err1.Error(), t)
+	err1e := err1.(*Error)
+	isEqual(http.StatusInternalServerError, err1e.HttpStatus(), t)
+
+	err2 := Smart(http.StatusBadRequest)
+	isEqual(MessageUnknownError, err2.Error(), t)
+	err2e := err2.(*Error)
+	isEqual(http.StatusBadRequest, err2e.HttpStatus(), t)
+
+	err3 := Smart("new err")
+	isEqual("new err", err3.Error(), t)
+	err3e := err3.(*Error)
+	isEqual(http.StatusInternalServerError, err3e.HttpStatus(), t)
+
+	err4 := Smart(http.StatusBadRequest, "bad req err")
+	isEqual("bad req err", err4.Error(), t)
+	err4e := err4.(*Error)
+	isEqual(http.StatusBadRequest, err4e.HttpStatus(), t)
+
+	err5 := Smart(http.StatusNotFound, "not found err", http.StatusBadRequest, "bad req err")
+	isEqual("not found err", err5.Error(), t)
+	err5e := err5.(*Error)
+	isEqual(http.StatusNotFound, err5e.HttpStatus(), t)
+
+	err6 := Smart(err4, err5)
+	isEqual("bad req err", err6.Error(), t)
+	err6e := err6.(*Error)
+	isEqual(http.StatusBadRequest, err6e.HttpStatus(), t)
+
+	err7 := Smart(err4, "another err")
+	isEqual("another err", err7.Error(), t)
+	err7e := err7.(*Error)
+	isEqual(err4e.HttpStatus(), err7e.HttpStatus(), t)
+
+	err8 := Smart("another err", err4)
+	isEqual("another err", err8.Error(), t)
+	err8e := err8.(*Error)
+	isEqual(err4e.HttpStatus(), err8e.HttpStatus(), t)
+
+	err9 := Smart(err4, http.StatusBadGateway)
+	isEqual(err4.Error(), err9.Error(), t)
+	err9e := err9.(*Error)
+	isEqual(http.StatusBadGateway, err9e.HttpStatus(), t)
+
+	err10 := Smart(http.StatusBadGateway, err4)
+	isEqual(err4.Error(), err10.Error(), t)
+	err10e := err10.(*Error)
+	isEqual(http.StatusBadGateway, err10e.HttpStatus(), t)
+
+}
+
+func isEqual(want, got interface{}, t *testing.T) {
+	if reflect.DeepEqual(want, got) {
+		return
+	}
+	_, file, line, _ := runtime.Caller(1)
+	splitted := strings.Split(file, string(os.PathSeparator))
+	t.Fatalf("%v:%v: Failed: got %v, want %v", splitted[len(splitted)-1], line, got, want)
+}
