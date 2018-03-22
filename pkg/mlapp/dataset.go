1526b46295c99f0e01f432fd5fa11a4797928d16
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-03-22 13:12:49 +0300

    Fix dataset rev in config

--- pkg/mlapp/dataset.go
+++ pkg/mlapp/dataset.go
@@ -5,10 +5,24 @@ func (c *BoardConfig) InjectDatasetRevisions(task *Task) {
 	for _, taskRev := range task.DatasetRevisions {
 		for _, v := range c.VolumesData {
 			if v.Name == taskRev.VolumeName {
+				if v.FlexVolume != nil {
+					_, ok := v.FlexVolume.Options["version"]
+					if ok {
+						v.FlexVolume.Options["version"] = taskRev.Revision
+					}
+				}
+			}
+		}
+	}
+
+	// Detect explicitly set revisions.
+	for _, taskRev := range task.DatasetRevisions {
+		for i, v := range c.Volumes {
+			if v.Name == taskRev.VolumeName {
 				if v.Dataset != nil {
-					v.Dataset.Version = taskRev.Revision
+					c.Volumes[i].Dataset.Version = taskRev.Revision
 				} else if v.DatasetFS != nil {
-					v.DatasetFS.Version = taskRev.Revision
+					c.Volumes[i].DatasetFS.Version = taskRev.Revision
 				}
 			}
 		}
