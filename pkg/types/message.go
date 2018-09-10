7db563134737964230cd0d800479c2ffe655469b
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-09-10 13:30:22 +0400

    Add unix nano timestamp to event message

--- pkg/types/message.go
+++ pkg/types/message.go
@@ -2,6 +2,7 @@ package types
 
 type Message struct {
 	ID      string      `json:"id"`
+	Ts      int64       `json:"ts"`
 	Type    string      `json:"type"`
 	Content interface{} `json:"content"`
 }
