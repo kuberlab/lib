e7dbe28627048dbfa3c625c6ad4e42f00f1c0a2a
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-10-22 17:17:37 +0400

    Add dealerclient model methods

---
+++ pkg/dealerclient/mlmodel.go
@@ -0,0 +1,83 @@
+package dealerclient
+
+import "fmt"
+
+type Model struct {
+	DisplayName   string
+	Name          string
+	Published     bool
+	WorkspaceName string
+}
+
+func (c *Client) DeleteModel(workspace, name string) error {
+	u := fmt.Sprintf("/workspace/%v/mlmodel/%v", workspace, name)
+
+	req, err := c.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return err
+	}
+	_, err = c.Do(req, nil)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) CreateModel(workspace, name string, public bool) error {
+	u := fmt.Sprintf("/workspace/%v/mlmodel", workspace)
+
+	ds := &Model{
+		Name:          name,
+		WorkspaceName: workspace,
+		Published:     public,
+		DisplayName:   name,
+	}
+	req, err := c.NewRequest("POST", u, ds)
+	if err != nil {
+		return err
+	}
+	_, err = c.Do(req, nil)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) CheckModel(workspace, name string) error {
+	u := fmt.Sprintf("/workspace/%v/mlmodel-check", workspace)
+
+	ds := &Model{
+		Name:          name,
+		WorkspaceName: workspace,
+		Published:     false,
+		DisplayName:   name,
+	}
+	req, err := c.NewRequest("POST", u, ds)
+	if err != nil {
+		return err
+	}
+	_, err = c.Do(req, nil)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) ListModels(workspace string) ([]Model, error) {
+	u := fmt.Sprintf("/workspace/%v/mlmodel", workspace)
+
+	var ds = make([]Model, 0)
+	req, err := c.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	_, err = c.Do(req, &ds)
+
+	if err != nil {
+		return nil, err
+	}
+	return ds, nil
+}
