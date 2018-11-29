f2587f242968c206ef33cedfe061f3d2c5e13d21
Vadim <monstarnn@gmail.com>
2018-11-29 10:42:09 +0300

    add description to serving spec param

--- pkg/mlapp/config.go
+++ pkg/mlapp/config.go
@@ -338,8 +338,9 @@ type ServingSpec struct {
 }
 
 type ServingSpecParam struct {
-	Name string `json:"name,omitempty"`
-	Type string `json:"type,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Type        string `json:"type,omitempty"`
+	Description string `json:"description,omitempty"`
 }
 
 func (s *Serving) Type() string {
